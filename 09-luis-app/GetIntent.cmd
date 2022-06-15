d6b3d0d467efc20ab705509610f5ecbb1435f6db
Phumin K <khumbal@hotmail.com>
2022-06-15 01:40:27 -0700

    module 10

--- 09-luis-app/GetIntent.cmd
+++ 09-luis-app/GetIntent.cmd
@@ -1,9 +1,9 @@
 @echo off
 
 rem Set values for your Language Understanding app
-set app_id=YOUR_APP_ID
-set endpoint=YOUR_ENDPOINT
-set key=YOUR_KEY
+set app_id=489f0c46-9699-4b5a-8ccb-41da5439c8ae
+set endpoint=https://lang-khumbal.cognitiveservices.azure.com/
+set key=e3819feb3ae1443ca7f7a16973cc32b8
 
 rem Get parameter and encode spaces for URL
 set input=%1
